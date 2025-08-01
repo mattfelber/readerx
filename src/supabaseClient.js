0cce30899fef1eef378d5655623cbc0821afaa64
Roguin <ro.guin31@gmail.com>
2025-07-31 20:20:03 -0400

    update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,6 @@
 // src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sgakgbzyxlkeimdrexiq.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnYWtnYnp5eGxrZWltZHJleGlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzMTk3NTIsImV4cCI6MjA2ODg5NTc1Mn0.WTnn4JCDhT0sJDbXGgJRnksHAvh2HkL7ZVEkcMm96m0";
-
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);

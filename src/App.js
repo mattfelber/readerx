419ecd9732e53e88dda87a51dd88d7705248a81d
Pedro Zelada <116367790+pedrozelada@users.noreply.github.com>
2025-08-02 23:13:06 -0400

    Revert "Roguin updates"

--- src/App.js
+++ src/App.js
@@ -1,63 +1,3 @@
-
-import React from "react";
-import { ThemeProvider, createTheme } from "@mui/material";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
-import PrivateRoute from "./PrivateRoute";
-import ApiTest from "./components/ApiTest";
-import TextReader from "./components/TextReader";
-import "./App.css";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#007AFF",
-    },
-    background: {
-      default: "#F5F5F7",
-    },
-  },
-  typography: {
-    fontFamily:
-      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
-  },
-  components: {
-    MuiPaper: {
-      defaultProps: {
-        elevation: 0,
-      },
-      styleOverrides: {
-        root: {
-          backgroundColor: "#ffffff",
-        },
-      },
-    },
-  },
-});
-
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard>
-                  <ApiTest />
-                  <TextReader />
-                </Dashboard>
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<Login />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
-
 import React from 'react';
 import { Box, AppBar, Toolbar, Typography } from '@mui/material';
 import { ThemeContextProvider } from './contexts/ThemeContext';
@@ -84,7 +24,6 @@ function App() {
         </div>
       </Box>
     </ThemeContextProvider>
-
   );
 }
 

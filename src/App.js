697e10245936f89d084b2b449070e3f76ebed607
Pedro Zelada <przs.gideon@gmail.com>
2025-08-06 21:43:55 -0400

    fixedapp

--- src/App.js
+++ src/App.js
@@ -11,33 +11,6 @@ import ApiTest from "./components/ApiTest";
 import TextReader from "./components/TextReader";
 import "./App.css";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#007AFF",
-    },
-    background: {
-      default: "#F5F5F7",
-    },
-  },
-  typography: {
-    fontFamily:
-      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
-  },
-  components: {
-    MuiPaper: {
-      defaultProps: {
-        elevation: 0,
-      },
-      styleOverrides: {
-        root: {
-          backgroundColor: "#ffffff",
-        },
-      },
-    },
-  },
-});
-
 function App() {
   return (
     <ThemeContextProvider>
